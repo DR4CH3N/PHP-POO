1a6cd2cfcf8e53f45f0428998b66b1f4bad99f1a
DR4CH3N <thehellc4t@gmail.com>
2022-06-06 11:16:52 -0300

    Create exemplo-geral.php

---
+++ exemplo-geral.php
@@ -0,0 +1,16 @@
+<?php 
+// exemplo-geral.php
+
+class exemplo {
+    // atributos ou propriedades
+
+    public string $nome;
+    public string $escola;
+}
+
+// criar objetos/instancias
+$exemploA = new exemplo;
+
+echo "<pre>";
+var_dump($exemploA);
+echo "<pre>";
