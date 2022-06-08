0d85d4fd6cd7af04945dfb105c9814c32bc3633a
DR4CH3N <thehellc4t@gmail.com>
2022-06-08 09:19:10 -0300

    Acesso direto aos recursos
    atribuição e leitura de dados em propriedades e metodos publicos

--- index.php
+++ index.php
@@ -7,12 +7,11 @@
     <title>Exemplo 1</title>
 </head>
 <body>
-    <h1>PHP POO - exemplo 2</h1>
+    <h1>PHP POO - exemplo 3</h1>
     <hr>
     <ul>
-        <li>Criação dos objetos</li>
-        <li>uso do metodo construtor</li>
-        <li>uso do <code>$this</code> para acessar as propriedades</li>
+        <li>Acesso direto a propriedades</li>
+        <li>Atribuição de dados e leitura</li>
     </ul>
 
     <?php
@@ -20,9 +19,33 @@
     require_once "src/Cliente.php";
 
     // criação dos objetos
-    $clienteA = new cliente('Tiago', 'tiago@gmail.com');
-    $clienteB = new cliente('Jon oliva', 'savatage@msn.com');
+    $clienteA = new cliente;
+    $clienteB = new cliente;
+
+    // atribuindo dados as propriedades do objeto
+
+    $clienteA-> nome = "tiago";
+    $clienteA-> email = "tiago@gmail.com";
+    $clienteA-> telefones = ["11-2135-0300","11-97777-5555"];
+
+    $clienteB-> nome = "bernardo";
+    $clienteB-> email = "bernardo@hotmail.com";
+    $clienteB-> telefones = array("11-98888-5555");
     ?>
+
+    <h2>dados dos objetos (leitura)</h2>
+    <h3> <?=$clienteA->nome ?></h3>
+    <p>Email: <?=$clienteA->email ?> </p>
+    <p>Telefones: <?=implode(", ",$clienteA->telefones) ?></p>
+    <p>Senha: <?=$clienteA->senha?></p>
+
+    <h3> <?=$clienteB->nome ?> </h3>
+    <p><?=$clienteB->email ?></p>
+    <p>Telefones: <?=implode(",",$clienteB->telefones)?></p>
+
+    <h2>Chamando o metodo exebirDados</h2>
+    <?= $clienteA->exibirDados() ?>
+    <?= $clienteB->exibirDados() ?>
 
     <pre> <?=var_dump($clienteA, $clienteB)?> </pre>
     
--- src/Cliente.php
+++ src/Cliente.php
@@ -3,15 +3,16 @@ class cliente {
     // propriedades (ou atributos)
     public string $nome;
     public string $email;
-    public string $senha;
+    public string $senha = "";
     public array $telefones;
 
-    // metodo construtor
-    public function __construct( string $nome, string $email )
-    {
-        /* $this é uma variavel especial, sepre apontando para a referencia da classe atualmente manipulada. */
-       $this->nome = $nome;
-       $this->email = $email;
-    }
 
+    // os pontos servem para poder concatenar
+    public function exibirDados() {
+        echo "<h3> $this->nome </h3>";
+        echo "<ul>";
+        echo "<li> $this->email </li> ";
+        echo "<li>". implode(',', $this->telefones). "</li>";
+        echo "</ul>";
+    }
 }
